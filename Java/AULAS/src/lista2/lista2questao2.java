6ce90715fc0db42832c12fe7082285f6a8e31914
Mariana Theml <marianatheml@gmail.com>
2021-10-04 16:01:36 -0300

    Correção da questão 2 da lista 2 de Java

--- Java/AULAS/src/lista2/lista2questao2.java
+++ Java/AULAS/src/lista2/lista2questao2.java
@@ -18,17 +18,17 @@ public class lista2questao2 {
 		System.out.print("Digite só mais um número inteiro: ");
 		num3 = leia.nextInt();
 		
-		if(num1<num2 && num1<num3 && num2<num3) {
+		if(num1<=num2 && num1<=num3 && num2<=num3) {
 			System.out.println("Os números digitados na ordem crescente são: "+num1+", "+num2+", "+num3);
-	    } else if (num1<num2 && num1<num3 && num2>num3) {
+	    } else if (num1<=num2 && num1<=num3 && num2>=num3) {
 	    	System.out.println("Os números digitados na ordem crescente são: "+num1+", "+num3+", "+num2);
-	    } else if (num2<num1 && num2<num3 && num1<num3) {
+	    } else if (num2<=num1 && num2<=num3 && num1<=num3) {
 	    	System.out.println("Os números digitados na ordem crescente são: "+num2+", "+num1+", "+num3);
-	    } else if (num2<num1 && num2<num3 && num3<num1) {
+	    } else if (num2<=num1 && num2<=num3 && num3<=num1) {
 	    	System.out.println("Os números digitados na ordem crescente são: "+num2+", "+num3+", "+num1);
-	    } else if (num3<num1 && num3<num2 && num2<num1) {
+	    } else if (num3<=num1 && num3<=num2 && num2<=num1) {
 	    	System.out.println("Os números digitados na ordem crescente são: "+num3+", "+num2+", "+num1);
-	    } else if (num3<num1 && num3<num2 && num1<num2) {
+	    } else if (num3<=num1 && num3<=num2 && num1<=num2) {
 	    	System.out.println("Os números digitados em ordem crescente são: "+num3+", "+num1+", "+num2);
 	    }
 	}	
